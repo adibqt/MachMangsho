94d12ec2d81c9900913c0bdac800915da459d931
Adib Rahman <adibrahman44@gmail.com>
2025-06-12 03:54:06 +0600

    Imported a tailwind ui

--- MachMangsho/src/context/AppContext.jsx
+++ MachMangsho/src/context/AppContext.jsx
@@ -1,6 +1,21 @@
+import React, { use } from "react";
+import { useNavigate } from "react-router-dom";
+
 export const AppContext = React.createContext();
 
 export const AppContextProvider = ({ children }) => {
+    const navigate = useNavigate();
+    const [user,setUser] = React.useState(null);
+    const [isSeller,setIsSeller] = React.useState(null);
 
+    
+    const value ={navigate, user, setUser, isSeller, setIsSeller};
+return <AppContext.Provider value={value}>
+    {children}
+  </AppContext.Provider>;   
 
+}
+
+export const useAppContext = () => {
+    return use(AppContext);
 }
