22dac345008ee338ebabaa2b7fb8f4b9ee46dbf6
Adib Rahman <adibrahman44@gmail.com>
2025-06-22 01:37:01 +0600

    Updated Homepage

--- MachMangsho/src/App.jsx
+++ MachMangsho/src/App.jsx
@@ -1,11 +1,16 @@
-
 import React from 'react'
 import Navbar from './components/Navbar'
-
+import { Routes, Route } from 'react-router-dom' // <-- Add Routes here
+import Home from './pages/Home' // <-- Import Home component
 const App = () => {
   return (
     <div>
       <Navbar />
+      <div className='px-6 md:px-16 lg:px-24 xl:px-32 '>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   )
 }
