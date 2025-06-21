edbcb856cc3df40b9e9215a8d4d314d1d603c410
Adib Rahman <adibrahman44@gmail.com>
2025-06-22 01:44:33 +0600

    Update App.jsx

--- MachMangsho/src/App.jsx
+++ MachMangsho/src/App.jsx
@@ -3,10 +3,15 @@ import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom' // <-- Add Routes here
 import Home from './pages/Home' // <-- Import Home component
 const App = () => {
+
+  const isSellerPath = window.location.pathname.includes("seller");
   return (
+  
     <div>
-      <Navbar />
-      <div className='px-6 md:px-16 lg:px-24 xl:px-32 '>
+      {isSellerPath ? null : <Navbar />}
+      <div className={`${
+        isSellerPath ? "" : 'px-6 md:px-16 lg:px-24 xl:px-32'
+      }`}>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
