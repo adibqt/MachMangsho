262596c1c869598318bbd4c92c4daa8a7db7bbdf
Adib Rahman <adibrahman44@gmail.com>
2025-06-21 22:59:59 +0600

    Changed frontend settings

--- MachMangsho/src/assets/assets.js
+++ MachMangsho/src/assets/assets.js
@@ -1,5 +1,5 @@
 
-import Mach from "./Mach.jpg";
+import Mach from "./Mach.svg";
 import search_icon from "./search_icon.svg";
 import remove_icon from "./remove_icon.svg";
 import arrow_right_icon_colored from "./arrow_right_icon_colored.svg";
