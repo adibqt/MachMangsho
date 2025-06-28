beed7d9464b15a3d3d393e70195d14a581f43df5
Adib Rahman <adibrahman44@gmail.com>
2025-06-29 00:41:00 +0600

    Update Categories.jsx

--- MachMangsho/src/components/Categories.jsx
+++ MachMangsho/src/components/Categories.jsx
@@ -3,13 +3,15 @@ import { assets } from '../assets/assets'
 
 const Categories = () => {
   return (
-    <div>
-       <p>Categories</p>
-       <div>
-        <div>
-            <img src={assets.box_icon} alt ="img"/>
-            <p>fruit</p> 
+    <div className='mt-16'>
+       <p className='text-2xl md:text-3XL font-medium'>Categories</p>
+       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-6 mt-6'>
+        
+        <div className='group cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col justify-center items-center'> 
+            <img src={assets.box_icon} alt ="img" className='group-hover:scale-108 transition max-w-28'/>
+            <p className='text-sm font-medium'>fruit</p> 
         </div>
+       
        </div>
     </div>
   )
