000e991616722d697127a212c051288428d13a3e
Adib Rahman <adibrahman44@gmail.com>
2025-07-12 09:43:37 +0600

    Add ProductCard component and use in BestSeller
    Created a new ProductCard component to display product details with interactive add/remove functionality. Integrated ProductCard into the BestSeller component to showcase a sample product.

--- MachMangsho/src/components/BestSeller.jsx
+++ MachMangsho/src/components/BestSeller.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import ProductCard from './ProductCard'
 
 const BestSeller = () => {
   return (
     <div className='mt-16'>
         <p className='text-2xl md:text-3xl font-medium'>BestSeller</p>
         <div>
-            
+          <ProductCard />  
         </div>
     </div>
   )
