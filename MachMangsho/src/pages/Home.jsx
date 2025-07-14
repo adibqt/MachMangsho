6b506e66d4412b1da4e3abf7926c598e1b64f3b5
Taseen007 <taseentajwar@iut-dhaka.edu>
2025-07-14 19:21:16 +0600

    Created the NextLetter.jsx

--- MachMangsho/src/pages/Home.jsx
+++ MachMangsho/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import MainBanner from '../components/MainBanner'
 import Categories from '../components/Categories'
 import BestSeller from '../components/BestSeller'
 import BottomBanner from '../components/BottomBanner'
+import NewsLetter from '../components/NewsLetter'
 
 const Home = () => {
   return (
@@ -12,6 +13,8 @@ const Home = () => {
       <Categories />
       <BestSeller />
       <BottomBanner />
+      <NewsLetter />
+      
     </div>
   )
 }
