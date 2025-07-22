e40ace166c5e3298e163b9a48d3a2d0b4ee91e16
Adib Rahman <adibrahman44@gmail.com>
2025-07-22 18:36:10 +0600

    Add Express server and update package scripts
    Initialized a basic Express server in server.js with a root endpoint. Updated package.json to set module type and added scripts for starting and running the server with nodemon.

---
+++ MachMangsho/server/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
