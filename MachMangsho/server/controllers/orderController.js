814f51895b15543c3a47dcaab18ea85b1753b9d8
Farhan Shahriar Haque <fsh33330@gmail.com>
2025-08-11 04:27:53 +0600

    completed stripe payment system

--- MachMangsho/server/controllers/orderController.js
+++ MachMangsho/server/controllers/orderController.js
@@ -1,6 +1,7 @@
 import Order from "../models/Order.js";
 import Product from "../models/Product.js";
 import stipe from "stripe";
+import User from "../models/User.js";
 
 
 //Place Order COD : /api/order/cod
@@ -110,25 +111,49 @@ export const placeOrderStripe = async (req, res) => {
 }
 
 
+export const stripeWebhook = async (request, response) => {
+    const stripeInstance = new stipe(process.env.STRIPE_SECRET_KEY);
+    const sig = request.headers['stripe-signature'];
+    let event;
 
+    try{
+        event = stripeInstance.webhooks.constructEvent(request.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+    } catch (error) {
+        response.status(400).send(`Webhook Error: ${error.message}`);
+        
+    }
 
+    switch(event.type) {
+        case 'payment_intent.succeeded': {
+            const paymentIntent = event.data.object;
+            const paymentIntentId = paymentIntent.id;
+           
+            const session = await stripeInstance.checkout.sessions.list({payment_intent : paymentIntentId,});
+            const { orderId, userId } = session.data[0].metadata;
+            await Order.findByIdAndUpdate(orderId, {isPaid: true})
+            await User.findByIdAndUpdate(userId, {cartItems: {}});   
 
+            break;
+        }
+         
+        case 'payment_intent.payment_failed': {
+          const paymentIntent = event.data.object;
+            const paymentIntentId = paymentIntent.id;
+           
+            const session = await stripeInstance.checkout.sessions.list({payment_intent : paymentIntentId,});
+            const { orderId, userId } = session.data[0].metadata;
+            break;
+        }
+        default:
+            console.error(`Unhandled event type ${event.type}`);
 
+            break;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    response.json({recieved: true})
+    
+    
+}
 
 // Get Orders by User ID : /api/order/user
 export const getUserOrders = async (req, res) => {
