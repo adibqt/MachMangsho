8f3f8c90b811d29aea7669d9463d61498475dfca
Adib Rahman <adibrahman44@gmail.com>
2025-08-22 23:06:13 +0600

    Add user login and authentication middleware
    Implemented a login endpoint in userController and userRoute, allowing users to authenticate with email and password. Added authUser middleware to verify JWT tokens from cookies for protected routes. Updated userRoute to include the new login route.

--- MachMangsho/server/controllers/userController.js
+++ MachMangsho/server/controllers/userController.js
@@ -38,3 +38,39 @@ export const register = async (req, res) => {
         return res.status(500).json({ success: false, message: "Internal server error" });
     }
 };
+
+//Login User: /api/user/login
+
+
+export const login = async (req, res) => { 
+    try {
+        const { email, password } = req.body;
+
+        if(!email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+        res.cookie('token', token, {
+            httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
+            secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+            sameSite:  process.env.NODE_ENV === 'production' ? 'None' : 'strict', //CSRF protection
+            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+        });
+
+        return res.json({ success: true, user: { email: user.email, name: user.name } });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ success: false, message: "Internal server error" });
+    }
+};
